7f97e7be7c5778914bbad92e668a39f93a06a41a
manucho254 <manuchoadero@gmail.com>
2021-11-08 19:15:17 +0300

    Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
+      <h3 class="is-bold is-size-3 mb-4">Latest Movies/</h3>
+   
     <div class="columns is-multiline">
      <div class="column is-3">
+     <router-link to="/about">  
       <div class="card">
        <div class="card-image">
-         <figure class="image is-4by2">
+         <figure class="image is-4by8">
+           <h3 class="is-dark m-2 is-success">Movie</h3>
           <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
          </figure>
        </div>
-       <div class="card-content">
+       <div class="card-content is-flex is-justify-content-space-between">
          <h3 class="is-dark">cool</h3>
+          <h3 class="is-dark">7.8</h3>
       </div>
       </div>
+     </router-link>  
      </div>
    </div>
    <!-- <figure class="image is-16by9">
